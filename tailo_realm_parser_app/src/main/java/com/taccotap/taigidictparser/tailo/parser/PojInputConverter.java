9b931cb4b70c24614ad48925fa4a8958378d79b9
shiami <shiami15@gmail.com>
2017-08-02 22:37:59 +0800

    v2.0.2 fix realm assets loader, fix poj

--- tailo_realm_parser_app/src/main/java/com/taccotap/taigidictparser/tailo/parser/PojInputConverter.java
+++ tailo_realm_parser_app/src/main/java/com/taccotap/taigidictparser/tailo/parser/PojInputConverter.java
@@ -2,19 +2,12 @@ package com.taccotap.taigidictparser.tailo.parser;
 
 import android.text.TextUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class PojInputConverter {
     private static final String TAG = PojInputConverter.class.getSimpleName();
-
-    // [o, a ,e ,u, i, n, m]
-    public static int[] sLomajiNumberToWordTempArray = {0, 0, 0, 0, 0, 0, 0};
-
-    public static void resetTempArray() {
-        int count = sLomajiNumberToWordTempArray.length;
-        for (int i = 0; i < count; i++) {
-            sLomajiNumberToWordTempArray[i] = 0;
-        }
-    }
 
     public static String convertPojNumberToPoj(String pojNumber) {
         String fixedPojNumber = ConverterUtils.fixLomajiNumber(pojNumber);
@@ -43,52 +36,70 @@ public class PojInputConverter {
     }
 
     private static int calculateTonePosition(String pojWithoutNumber) {
-        resetTempArray();
-
+        pojWithoutNumber = pojWithoutNumber.toLowerCase();
         int count = pojWithoutNumber.length();
-        for (int i = 0; i < count; i++) {
-            final char c = pojWithoutNumber.charAt(i);
 
-            switch (c) {
-                case 'o':
-                case 'O':
-                    sLomajiNumberToWordTempArray[0] = i + 1;
-                    break;
-                case 'a':
-                case 'A':
-                    sLomajiNumberToWordTempArray[1] = i + 1;
-                    break;
-                case 'e':
-                case 'E':
-                    sLomajiNumberToWordTempArray[2] = i + 1;
-                    break;
-                case 'u':
-                case 'U':
-                    sLomajiNumberToWordTempArray[3] = i + 1;
-                    break;
-                case 'i':
-                case 'I':
-                    sLomajiNumberToWordTempArray[4] = i + 1;
-                    break;
-                case 'n':
-                case 'N':
-                    sLomajiNumberToWordTempArray[5] = i + 1;
-                    break;
-                case 'm':
-                case 'M':
-                    sLomajiNumberToWordTempArray[6] = i + 1;
-                    break;
+        final int lastIndexOfVowel = lastIndexOfRegex(pojWithoutNumber, "a|i|u|e|o");
+        if (lastIndexOfVowel == -1) {
+            final int lastIndexOfHalfVowel = lastIndexOfRegex(pojWithoutNumber, "m|ng|n");
+            if (lastIndexOfHalfVowel == -1) {
+                return -1;
+            } else {
+                return lastIndexOfHalfVowel;
+            }
+        } else {
+            if (count == 1) {
+                return 0;
+            } else {
+                if (lastIndexOfVowel == 0) {
+                    return 0;
+                } else {
+                    if (pojWithoutNumber.contains("oai") && pojWithoutNumber.endsWith("h")) {
+                        return pojWithoutNumber.indexOf("o");
+                    }
+
+                    final String previousChar = pojWithoutNumber.substring(count - 2, count - 1);
+                    // if vowel count >= 2
+                    if (previousChar.matches("a|i|u|e|o")) {
+                        // if vowel is the last char
+                        if (lastIndexOfVowel + 1 == count) {
+                            if (previousChar.equals("i")) {
+                                return lastIndexOfVowel;
+                            } else {
+                                return lastIndexOfVowel - 1;
+                            }
+                        } else {
+                            return lastIndexOfVowel;
+                        }
+                    } else {
+                        return lastIndexOfVowel;
+                    }
+                }
             }
         }
-        int foundToneCharPosition = -1;
-        for (final int pos : sLomajiNumberToWordTempArray) {
-            if (pos > 0) {
-                foundToneCharPosition = pos - 1;
-                break;
-            }
+    }
+
+    /**
+     * Version of lastIndexOf that uses regular expressions for searching.
+     * By Tomer Godinger.
+     *
+     * @param str    String in which to search for the pattern.
+     * @param toFind Pattern to locate.
+     * @return The index of the requested pattern, if found; NOT_FOUND (-1) otherwise.
+     */
+    public static int lastIndexOfRegex(String str, String toFind) {
+        Pattern pattern = Pattern.compile(toFind);
+        Matcher matcher = pattern.matcher(str);
+
+        // Default to the NOT_FOUND constant
+        int lastIndex = -1;
+
+        // Search for the given pattern
+        while (matcher.find()) {
+            lastIndex = matcher.start();
         }
 
-        return foundToneCharPosition;
+        return lastIndex;
     }
 
     private static String generatePojInput(String pojWithoutNumber, String number, int tonePosition) {
@@ -101,7 +112,11 @@ public class PojInputConverter {
             if (i == tonePosition) {
                 String pojNumber = currentCharString + number;
                 final String poj = Poj.sPojNumberToPojUnicodeHashMap.get(pojNumber);
-                stringBuilder.append(poj);
+                if (poj != null) {
+                    stringBuilder.append(poj);
+                } else {
+                    stringBuilder.append(currentCharString);
+                }
             } else {
                 stringBuilder.append(currentCharString);
             }
